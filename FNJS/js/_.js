d31fea41d703c44b9d8f68632eddeb3e5738672a
강수성 <mercuriver@gmail.com>
2021-04-15 21:40:32 +0900

    refactor: _.js 파일 정리

--- FNJS/js/_.js
+++ FNJS/js/_.js
@@ -20,3 +20,27 @@ function _map(list, mapper) {
   });
   return new_list;
 }
+
+function _curry(fn) {
+  return function (a, b) {
+    return arguments.length == 2
+      ? fn(a, b)
+      : function (b) {
+          return fn(a, b);
+        };
+  };
+}
+
+function _curryr(fn) {
+  return function (a, b) {
+    return arguments.length == 2
+      ? fn(a, b)
+      : function (b) {
+          return fn(b, a);
+        };
+  };
+}
+
+var _get = _curryr(function (obj, key) {
+  return obj == null ? undefined : obj[key];
+});
